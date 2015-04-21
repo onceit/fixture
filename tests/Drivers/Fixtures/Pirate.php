44d7004d73a4120f0b12a3fccc1522c0431ca4c6
Benjamin Haines <ben@onceit.co.nz>
2015-04-21 17:30:34 +1200

    Set correct table var

--- tests/Drivers/Fixtures/Pirate.php
+++ tests/Drivers/Fixtures/Pirate.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Pirate extends Model
 {
-    protected $tableName = 'pirates';
+    protected $table = 'pirates';
 
     public function parrot()
     {
